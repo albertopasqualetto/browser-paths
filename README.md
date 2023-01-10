9477ec291540f02b3eb1127d07a4603c36715156
Alberto Pasqualetto <39854348+albertopasqualetto@users.noreply.github.com>
2023-01-10 16:33:21 +0100

    If not found, return null

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A simple package that retrieves the path of the most known browsers (if installed), no matter the OS.
 
+If browser is not found, it returns null.
+
+Other browsers (Opera, Brave, ...) will be added in future, feel free to help me!
+
 ## Supported Browsers
 
 - Google Chrome
--- index.js
+++ index.js
@@ -4,22 +4,22 @@ macos = require('./macos.js');
 
 
 function getChromePath() {
-	return windows.getChrome() || linux.getChrome() || macos.getChrome();
+	return windows.getChrome() || linux.getChrome() || macos.getChrome() || null;
 }
 
 function getEdgePath() {
-	return windows.getEdge() || linux.getEdge() || macos.getEdge();
+	return windows.getEdge() || linux.getEdge() || macos.getEdge() || null;
 }
 
 function getChromiumPath() {
 	//If not found, fallbacks to another Chromium-based browser
 	//(only on linux there is a "real" chromium possible install path)
-	return windows.getChromium() || linux.getChromium() || macos.getChromium();
+	return windows.getChromium() || linux.getChromium() || macos.getChromium() || null;
 }
 
 
 function getFirefoxPath() {
-	return windows.getFirefox() || linux.getFirefox() || macos.getFirefox();
+	return windows.getFirefox() || linux.getFirefox() || macos.getFirefox() || null;
 }
 
 
